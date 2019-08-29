c46fcd6183907cfd07d736603df4dcb24bb2565d
gpsgui <gp_simao@hotmail.com>
2019-08-28 22:56:19 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <button onclick={()=>{
+          const exec = require('child_process').exec;
+          console.log(exec("lsblk | grep /media | grep -oP "sd[a-z]" | awk '{print "/dev/"$1}' | sort | uniq");)
+        }}>Listar Volumes</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
